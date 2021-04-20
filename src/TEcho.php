538f1d717d3bb0ee405baabe4f2f6cd046b234ba
zxy <zxy_200909@126.com>
2021-04-20 11:47:41 +0800

    init project

---
+++ src/TEcho.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace techo;
+class TEcho {
+    public static function echo($s) {
+        echo $s;
+    }
+}
---
+++ tests/t.php
@@ -0,0 +1,7 @@
+<?php
+
+require "../vendor/autoload.php";
+
+use techo\TEcho;
+
+TEcho::echo("ssss");
